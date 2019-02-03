40b28033b5078bbf88cceb9437215bd7b99f0a3d
batbien <we50ygul@gmail.com>
2019-02-03 23:14:55 +0100

    fix node_env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,8 @@
-var node_env = process.env.NODE_ENV;
-
+var node_env = process.env.NODE_ENV || "development";
 
 if (node_env === "development" || node_env === "test") {
   var config = require("./config.json");
   Object.keys(config[node_env]).forEach(key => {
     process.env[key] = config[node_env][key];
   });
-} 
+}
